d5e598acd98a787e66a00e0a4b550dc2bcbdab7a
Nd Daniel <ndondadaniel2020@gmail.com>
2022-11-25 01:22:58 +0100

    comentei  as funções

--- System/widgests/Criptografia.py
+++ System/widgests/Criptografia.py
@@ -1,11 +1,16 @@
+ancii = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4",
+             "5", "6",
+             "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "À", "Á", "Ã", "B", "C", "Ç", "D", "E", "È",
+             "É", "F",
+             "G", "H", "I", "Ì", "Í", "J", "K", "L", "M", "N", "O", "Ò", "Ó", "Õ", "P", "Q", "R", "S", "T", "U",
+             "Ù", "Ú",
+             "V", "W", "X", "Y", "Z", "[", "^", "_", "a", "à", "á", "ã", "b", "c", "d", "e", "è", "é", "f", "g",
+             "h", "i",
+             "ì", "í", "j", "k", "l", "m", "n", "o", "ò", "ó", "õ", "p", "q", "r", "s", "t", "u", "ù", "ú", "v",
+             "w", "x",
+             "y", "z", "{", "|", "}", "~", '"']
 
-ancii = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6",
-         "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "À", "Á", "Ã", "B", "C", "Ç", "D", "E", "È", "É", "F",
-         "G", "H", "I", "Ì", "Í", "J", "K", "L", "M", "N", "O", "Ò", "Ó", "Õ", "P", "Q", "R", "S", "T", "U", "Ù", "Ú",
-         "V", "W", "X", "Y", "Z", "[", "^", "_", "a", "à", "á", "ã", "b", "c", "d", "e", "è", "é", "f", "g", "h", "i",
-         "ì", "í", "j", "k", "l", "m", "n", "o", "ò", "ó", "õ", "p", "q", "r", "s", "t", "u", "ù", "ú", "v", "w", "x",
-         "y", "z", "{", "|", "}", "~", '"']
-
+# esta fução e responsavel corrigir o erro de pedir um número positivo
 def corrigirErroNamberPositivo(n):
 
     while True:
@@ -13,58 +18,59 @@ def corrigirErroNamberPositivo(n):
             a = ancii[n]
             return n
         except:
-            n = n-(len(ancii)-1)
+            n = n - (len(ancii) - 1)
             return n
 
+# esta fução e responsavel corrigir o erro de pedir um número negativo
 def corrigirErroNamberNegativo(n):
     a = ancii[n]
     a = ordSelf(a)
     return a
 
 # converter caracter para numero
-def ordSelf (getCaract):
+def ordSelf(getCaract):
     for n, caracter in enumerate(ancii):
         if getCaract == caracter:
             return n
 
 # converter de cnúmero para caracter
-def chrSelf (getNun):
+def chrSelf(getNun):
     for n, caracter in enumerate(ancii):
         if getNun == n:
             return caracter
 
 # criptogáfar
-def criptografar(chave, mensagem):
-    n =128
-    novaMsm = ''
-    for letra in mensagem:
-        indice = ordSelf(letra)
+def criptografar(caracteres):
+    chave = len(caracteres)
+    caracterNovos = ''
+    for caracter in caracteres:
+        indice = ordSelf(caracter)
 
-        novaLetra = (indice+chave)
+        valorNovaLetra = (indice + chave)
 
         while True:
             try:
-                novaMsm += chrSelf(novaLetra)
+                caracterNovos += chrSelf(valorNovaLetra)
                 break
             except:
-                novaLetra = corrigirErroNamberPositivo(novaLetra)
+                valorNovaLetra = corrigirErroNamberPositivo(valorNovaLetra)
 
-    return novaMsm
+    return caracterNovos
 
 # descriptogáfar
-def descriptografar(chave, mensagem):
-    n = 128
-    novaMsm = ''
-    for letra in mensagem:
+def descriptografar(caracteres):
+    chave = len(caracteres)
+    caracterNovos = ''
+    for letra in caracteres:
         indice = ordSelf(letra)
 
-        novaLetra = (indice - chave)
+        valorNovaLetra = (indice - chave)
 
         while True:
             try:
-                novaMsm += chrSelf(novaLetra)
+                caracterNovos += chrSelf(valorNovaLetra)
                 break
             except:
-                novaLetra = corrigirErroNamberNegativo(novaLetra)
+                valorNovaLetra = corrigirErroNamberNegativo(valorNovaLetra)
 
-    return novaMsm
+    return caracterNovos
