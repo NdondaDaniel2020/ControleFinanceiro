a1f99f497dd7a94330598be60438c93bd6a96f44
Nd Daniel <ndondadaniel2020@gmail.com>
2022-11-27 05:10:25 +0100

    fiz os testes da funcionablidade doslide do skadwidget

--- test z/slide.py
+++ test z/slide.py
@@ -3,7 +3,7 @@ import sys
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
-from PySide6.QtCore import *
+
 
 from tests import Ui_MainWindow
 from stackedWidgets import SlidingStackedWidget
@@ -17,37 +17,30 @@ class Test (QMainWindow):
         self.slide = SlidingStackedWidget()
         self.slide.setAnimation(QEasingCurve.OutCirc)
 
-
         self.widget = QWidget()
         self.widget.setObjectName(u"frame_2")
         self.widget.setStyleSheet("background-color: rgb(85, 255, 255);")
-        self.verticalLayout = QVBoxLayout(self.widget)
-        self.verticalLayout.setObjectName(u"verticalLayout_2")
 
         self.widget_2 = QWidget()
         self.widget_2.setObjectName(u"frame_2")
         self.widget_2.setStyleSheet("background-color: rgb(255, 0, 0);")
-        self.verticalLayout_2 = QVBoxLayout(self.widget_2)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
 
         self.widget_3 = QWidget()
         self.widget_3.setObjectName(u"frame_2")
         self.widget_3.setStyleSheet("background-color: rgb(85, 255, 127);")
-        self.verticalLayout_3 = QVBoxLayout(self.widget_3)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_2")
 
         self.widget_4 = QWidget()
         self.widget_4.setObjectName(u"frame_2")
         self.widget_4.setStyleSheet("background-color: rgb(255, 255, 0);")
-        self.verticalLayout_4 = QVBoxLayout(self.widget_4)
-        self.verticalLayout_4.setObjectName(u"verticalLayout_2")
-
 
         self.slide.addWidget(self.widget)
         self.slide.addWidget(self.widget_2)
         self.slide.addWidget(self.widget_3)
         self.slide.addWidget(self.widget_4)
 
+        self.widget_5 = QWidget()
+        self.widget_5.setObjectName(u"frame_2")
+        self.widget_5.setStyleSheet("background-color: rgb(170, 255, 255);")
 
         self.ui.verticalLayout_2.addWidget(self.slide)
 
@@ -56,9 +49,10 @@ class Test (QMainWindow):
         self.ui.pushButton.clicked.connect(lambda: self.slide.slideInIdx(0))
         self.ui.pushButton_2.clicked.connect(lambda: self.slide.slideInIdx(1))
         self.ui.pushButton_3.clicked.connect(lambda: self.slide.slideInIdx(2))
-        self.ui.pushButton_4.clicked.connect(lambda: self.slide.slideInIdx(3))
+        self.ui.pushButton_4.clicked.connect(lambda: self.tesInwdt())
 
         self.show()
+
 
     def testSlide(self):
         self.slide.setDirection(Qt.Vertical)
